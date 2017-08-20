42fedc040e687ccf6d039dd8422b5ef71722709d
izabela <izabel.kulak@gmail.com>
2017-08-20 18:04:56 +0200

    sdfsdf

--- client.js
+++ client.js
@@ -1,14 +1,14 @@
 console.log("hej")
-window.onload = function () {
-    var button = document.getElementById("button");
-    button.addEventListener("click", function (event) {
-        console.log("Elo");
-        event.preventDefault();
-
-        var polskie = document.getElementById("polskie").value
-        var hiszp = document.getElementById("hiszp").value
-        console.log(polskie)
-        console.log(hiszp)
-
-    })
-}
+// window.onload = function () {
+//     var button = document.getElementById("button");
+//     button.addEventListener("click", function (event) {
+//         console.log("Elo");
+//         event.preventDefault();
+//
+//         var polskie = document.getElementById("polskie").value
+//         var hiszp = document.getElementById("hiszp").value
+//         console.log(polskie)
+//         console.log(hiszp)
+//
+//     })
+// }
--- index.js
+++ index.js
@@ -3,12 +3,42 @@ var app = express();
 var http = require('http').Server(app);
 var port = process.env.PORT || 3000;
 
+const bodyParser = require('body-parser');
 app.use(express.static(__dirname + '/'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); // to support URL-encoded bodies
+app.use(bodyParser.urlencoded()); // to support URL-encoded bodies
+
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'fiszki'
+});
+
+connection.connect()
+
+app.post('/', function(req, res) {
+    console.log(`Update database with words: ${req.body.p}, ${req.body.h}`);
+    console.log(req.body.h);
+
+    connection.query(`INSERT INTO words (polish, spanish) VALUES('${req.body.p}', '${req.body.h}');`, function (err, rows, fields) {
+        if (err) throw err
+    })
+    res.sendFile(__dirname + '/main.html');
+});
+
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    console.log('i co');
+    res.sendFile(__dirname + '/main.html');
 });
 
 http.listen(port, function(){
     console.log('listening on *:' + port);
 });
+
+
+
+
